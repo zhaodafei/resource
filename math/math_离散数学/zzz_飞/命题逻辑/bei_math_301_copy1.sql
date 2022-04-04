4bad295667ebcf1e4c6b5ab6283c02601d55ecb8
zhaodafei111 <1097625354@qq.com>
2022-04-04 16:51:01 +0800

    init

---
+++ math/math_离散数学/zzz_飞/命题逻辑/bei_math_301_copy1.sql
@@ -0,0 +1,34 @@
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bei_math_301_copy1
+-- ----------------------------
+DROP TABLE IF EXISTS `bei_math_301_copy1`;
+CREATE TABLE `bei_math_301_copy1`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `fields_1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `fields_2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `fields_3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `compute_1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `compute_2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `compute_3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `compute_4` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `other_sql` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of bei_math_301_copy1
+-- ----------------------------
+INSERT INTO `bei_math_301_copy1` VALUES ('序号', 'p', 'q', 'r', NULL, NULL, NULL, NULL, 'SELECT fields_1,fields_2,fields_3 FROM bei_math_301 GROUP BY fields_1,fields_2,fields_3;');
+INSERT INTO `bei_math_301_copy1` VALUES ('1', '0', '0', '0', NULL, NULL, NULL, NULL, '这是校验是否有写错的值');
+INSERT INTO `bei_math_301_copy1` VALUES ('2', '0', '0', '1', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `bei_math_301_copy1` VALUES ('3', '0', '1', '0', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `bei_math_301_copy1` VALUES ('4', '0', '1', '1', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `bei_math_301_copy1` VALUES ('5', '1', '0', '0', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `bei_math_301_copy1` VALUES ('6', '1', '0', '1', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `bei_math_301_copy1` VALUES ('7', '1', '1', '0', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `bei_math_301_copy1` VALUES ('8', '1', '1', '1', NULL, NULL, NULL, NULL, NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
